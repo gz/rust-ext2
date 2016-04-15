50a53d62c01587bb814259f96c4c8a727c4365fa
Gerd Zellweger <mail@gerdzellweger.com>
2016-04-15 08:11:59 +0200

    Add more structures.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- src/lib.rs
+++ src/lib.rs
@@ -5,66 +5,66 @@
 
 
 use core::mem::{size_of, transmute, zeroed};
-use core::str;
-use core::slice;
-use core::default;
+//use core::str;
+//use core::slice;
+//use core::default;
 use core::fmt;
 
 #[macro_use]
 extern crate std;
 
-#[repr(C, packed)]
-pub struct Ext2SuperBlock {
+#[repr(C)]
+pub struct SuperBlock {
     /// Inodes count
-    pub s_inodes_count: u32,
+    pub inodes_count: u32,
     /// Blocks count
-    pub s_blocks_count: u32,
+    pub blocks_count: u32,
     /// Reserved blocks count
-    pub s_r_blocks_count: u32,
+    pub r_blocks_count: u32,
     /// Free blocks count
-    pub s_free_blocks_count: u32,
+    pub free_blocks_count: u32,
     /// Free inodes count
-    pub s_free_inodes_count: u32,
+    pub free_inodes_count: u32,
     /// First Data Block
-    pub s_first_data_block: u32,
+    pub first_data_block: u32,
     /// Block size
-    pub s_log_block_size: u32,
+    pub log_block_size: u32,
     /// Fragment size
-    pub s_log_frag_size: u32,
+    pub log_frag_size: u32,
     /// # Blocks per group
-    pub s_blocks_per_group: u32,
+    pub blocks_per_group: u32,
     /// # Fragments per group
-    pub s_frags_per_group: u32,
+    pub frags_per_group: u32,
     /// # Inodes per group
-    pub s_inodes_per_group: u32,
+    pub inodes_per_group: u32,
     /// Mount time
-    pub s_mtime: u32,
+    pub mtime: u32,
     /// Write time
-    pub s_wtime: u32,
+    pub wtime: u32,
     /// Mount count
-    pub s_mnt_count: u16,
+    pub mnt_count: u16,
     /// Maximal mount count
-    pub s_max_mnt_count: u16,
+    pub max_mnt_count: u16,
     /// Magic signature
-    pub s_magic: u16,
+    pub magic: u16,
     /// File system state
-    pub s_state: u16,
+    pub state: u16,
     /// Behaviour when detecting errors
-    pub s_errors: u16,
+    pub errors: u16,
     /// minor revision level
-    pub s_minor_rev_level: u16,
+    pub minor_rev_level: u16,
     /// time of last check
-    pub s_lastcheck: u32,
+    pub lastcheck: u32,
     /// max. time between checks
-    pub s_checkinterval: u32,
+    pub checkinterval: u32,
     /// OS
-    pub s_creator_os: u32,
+    pub creator_os: u32,
     /// Revision level
-    pub s_rev_level: u32,
+    pub rev_level: u32,
     /// Default uid for reserved blocks
-    pub s_def_resuid: u16,
+    pub def_resuid: u16,
     /// Default gid for reserved blocks
-    pub s_def_resgid: u16,
+    pub def_resgid: u16,
 
 	/*
 	 * These fields are for EXT2_DYNAMIC_REV superblocks only.
@@ -79,75 +79,220 @@ pub struct Ext2SuperBlock {
 	 * feature set, it must abort and not try to meddle with
 	 * things it doesn't understand...
 	 */
-	s_first_ino: u32, 		/* First non-reserved inode */
-	s_inode_size: u16, 		/* size of inode structure */
-	s_block_group_nr: u16, 	/* block group # of this superblock */
-	s_feature_compat: u32, 	/* compatible feature set */
-	s_feature_incompat: u32, 	/* incompatible feature set */
-	s_feature_ro_compat: u32, 	/* readonly-compatible feature set */
-	s_uuid: [u8; 16],		/* 128-bit uuid for volume */
-	s_volume_name: [u8; 16], 	/* volume name */
-	s_last_mounted: [u8; 64], 	/* directory where last mounted */
-	s_algorithm_usage_bitmap: u32, /* For compression */
+     pub first_ino: u32, 		/* First non-reserved inode */
+     pub inode_size: u16, 		/* size of inode structure */
+     pub block_group_nr: u16, 	/* block group # of this superblock */
+     pub feature_compat: u32, 	/* compatible feature set */
+     pub feature_incompat: u32, 	/* incompatible feature set */
+     pub feature_ro_compat: u32, 	/* readonly-compatible feature set */
+     pub uuid: [u8; 16],		/* 128-bit uuid for volume */
+     pub volume_name: [u8; 16], 	/* volume name */
+     pub last_mounted: [u8; 64], 	/* directory where last mounted */
+     pub algorithm_usage_bitmap: u32, /* For compression */
 
 	/*
 	 * Performance hints.  Directory preallocation should only
 	 * happen if the EXT2_COMPAT_PREALLOC flag is on.
 	 */
-	s_prealloc_blocks: u8,	/* Nr of blocks to try to preallocate*/
-	s_prealloc_dir_blocks: u8,	/* Nr to preallocate for dirs */
-	s_padding1: u16,
+     pub prealloc_blocks: u8,	/* Nr of blocks to try to preallocate*/
+     pub prealloc_dir_blocks: u8,	/* Nr to preallocate for dirs */
+     pub padding1: u16,
 
 	/*
 	 * Journaling support valid if EXT3_FEATURE_COMPAT_HAS_JOURNAL set.
 	 */
-	s_journal_uuid: [u8; 16],	/* uuid of journal superblock */
-	s_journal_inum: u32,		/* inode number of journal file */
-	s_journal_dev: u32,		/* device number of journal file */
-	s_last_orphan: u32,		/* start of list of inodes to delete */
-	s_hash_seed: [u32; 4],		/* HTREE hash seed */
-	s_def_hash_version: u8,	/* Default hash version to use */
-	s_reserved_char_pad: u8,
-	s_reserved_word_pad: u16,
-	s_default_mount_opts: u32,
- 	s_first_meta_bg: u32, 	/* First metablock block group */
-	s_reserved: [u32; 190],	/* Padding to the end of the block */
+     pub journal_uuid: [u8; 16],	/* uuid of journal superblock */
+     pub journal_inum: u32,		/* inode number of journal file */
+     pub journal_dev: u32,		/* device number of journal file */
+     pub last_orphan: u32,		/* start of list of inodes to delete */
+     pub hash_seed: [u32; 4],		/* HTREE hash seed */
+     pub def_hash_version: u8,	/* Default hash version to use */
+     pub reserved_char_pad: u8,
+     pub reserved_word_pad: u16,
+     pub default_mount_opts: u32,
+     pub first_meta_bg: u32, 	/* First metablock block group */
+     pub reserved: [u32; 190],	/* Padding to the end of the block */
 }
 
-impl core::default::Default for Ext2SuperBlock {
-    fn default() -> Ext2SuperBlock {
+impl core::default::Default for SuperBlock {
+    fn default() -> SuperBlock {
         unsafe { zeroed() }
     }
 }
 
-impl core::fmt::Debug for Ext2SuperBlock {
+impl core::fmt::Debug for SuperBlock {
     fn fmt(&self, f: &mut core::fmt::Formatter) -> fmt::Result {
-        try!(write!(f, "Ext2SuperBlock\n"));
-        try!(write!(f, "\tInodes count: {}\n", self.s_inodes_count));
-        try!(write!(f, "\tBlocks count: {}\n", self.s_blocks_count));
-        try!(write!(f, "\tReserved blocks count: {}\n", self.s_r_blocks_count));
-        try!(write!(f, "\tFree blocks count: {}\n", self.s_free_blocks_count));
-        try!(write!(f, "\tFree inodes count: {}\n", self.s_free_inodes_count));
-        try!(write!(f, "\tFirst Data Block: {}\n", self.s_first_data_block));
-        try!(write!(f, "\tBlock size: {}\n", self.s_log_block_size));
-        try!(write!(f, "\tFragment size: {}\n", self.s_log_frag_size));
-        try!(write!(f, "\t# Blocks per group: {}\n", self.s_blocks_per_group));
-        try!(write!(f, "\t# Fragments per group: {}\n", self.s_frags_per_group));
-        try!(write!(f, "\t# Inodes per group: {}\n", self.s_inodes_per_group));
-        try!(write!(f, "\tMount time: {}\n", self.s_mtime));
-        try!(write!(f, "\tWrite time: {}\n", self.s_wtime));
-        try!(write!(f, "\tMount count: {}\n", self.s_mnt_count));
-        try!(write!(f, "\tMaximal mount count: {}\n", self.s_max_mnt_count));
-        try!(write!(f, "\tMagic signature: {}\n", self.s_magic));
-        try!(write!(f, "\tFile system state: {}\n", self.s_state));
-        try!(write!(f, "\tBehaviour when detecting errors: {}\n", self.s_errors));
-        try!(write!(f, "\tminor revision level: {}\n", self.s_minor_rev_level));
-        try!(write!(f, "\ttime of last check: {}\n", self.s_lastcheck));
-        try!(write!(f, "\tmax. time between checks: {}\n", self.s_checkinterval));
-        try!(write!(f, "\tOS: {}\n", self.s_creator_os));
-        try!(write!(f, "\tRevision level: {}\n", self.s_rev_level));
-        try!(write!(f, "\tDefault uid for reserved blocks: {}\n", self.s_def_resuid));
-        write!(f, "\tDefault gid for reserved blocks: {}\n", self.s_def_resgid)
+        try!(write!(f, "SuperBlock\n"));
+        try!(write!(f, "\tInodes count: {}\n", self.inodes_count));
+        try!(write!(f, "\tBlocks count: {}\n", self.blocks_count));
+        try!(write!(f, "\tReserved blocks count: {}\n", self.r_blocks_count));
+        try!(write!(f, "\tFree blocks count: {}\n", self.free_blocks_count));
+        try!(write!(f, "\tFree inodes count: {}\n", self.free_inodes_count));
+        try!(write!(f, "\tFirst Data Block: {}\n", self.first_data_block));
+        try!(write!(f, "\tBlock size: {}\n", self.log_block_size));
+        try!(write!(f, "\tFragment size: {}\n", self.log_frag_size));
+        try!(write!(f, "\t# Blocks per group: {}\n", self.blocks_per_group));
+        try!(write!(f, "\t# Fragments per group: {}\n", self.frags_per_group));
+        try!(write!(f, "\t# Inodes per group: {}\n", self.inodes_per_group));
+        try!(write!(f, "\tMount time: {}\n", self.mtime));
+        try!(write!(f, "\tWrite time: {}\n", self.wtime));
+        try!(write!(f, "\tMount count: {}\n", self.mnt_count));
+        try!(write!(f, "\tMaximal mount count: {}\n", self.max_mnt_count));
+        try!(write!(f, "\tMagic signature: {}\n", self.magic));
+        try!(write!(f, "\tFile system state: {}\n", self.state));
+        try!(write!(f, "\tBehaviour when detecting errors: {}\n", self.errors));
+        try!(write!(f, "\tminor revision level: {}\n", self.minor_rev_level));
+        try!(write!(f, "\ttime of last check: {}\n", self.lastcheck));
+        try!(write!(f, "\tmax. time between checks: {}\n", self.checkinterval));
+        try!(write!(f, "\tOS: {}\n", self.creator_os));
+        try!(write!(f, "\tRevision level: {}\n", self.rev_level));
+        try!(write!(f, "\tDefault uid for reserved blocks: {}\n", self.def_resuid));
+        write!(f, "\tDefault gid for reserved blocks: {}\n", self.def_resgid)
 
+    }
+}
+
+#[repr(C)]
+struct GroupDesc {
+     /// Blocks bitmap block
+     block_bitmap: u32,
+     /// Inodes bitmap block
+     inode_bitmap: u32,
+     /// Inodes table block
+     inode_table: u32,
+     /// Free blocks count
+     free_blocks_count: u16,
+     /// Free inodes count
+     free_inodes_count: u16,
+     /// Directories count
+     used_dirs_count: u16,
+     pad: u16,
+     reserved: [u32; 3]
+}
+
+impl core::default::Default for GroupDesc {
+    fn default() -> GroupDesc {
+        unsafe { zeroed() }
+    }
+}
+
+impl core::fmt::Debug for GroupDesc {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> fmt::Result {
+        try!(write!(f, "GroupDesc\n"));
+        try!(write!(f, "\tBlocks bitmap block: {}\n", self.block_bitmap));
+        try!(write!(f, "\tInodes bitmap block: {}\n", self.inode_bitmap));
+        try!(write!(f, "\tInodes table block: {}\n", self.inode_table));
+        try!(write!(f, "\tFree blocks count: {}\n", self.free_blocks_count));
+        try!(write!(f, "\tFree inodes count: {}\n", self.free_inodes_count));
+        write!(f, "\tDirectories count: {}\n", self.used_dirs_count)
+    }
+}
+
+
+const EXT2_NDIR_BLOCKS: usize = 12;
+const EXT2_IND_BLOCK: usize = EXT2_NDIR_BLOCKS;
+const EXT2_DIND_BLOCK: usize = (EXT2_IND_BLOCK + 1);
+const EXT2_TIND_BLOCK: usize = (EXT2_DIND_BLOCK + 1);
+const EXT2_N_BLOCKS: usize = (EXT2_TIND_BLOCK + 1);
+
+pub struct INode {
+     /// File mode
+     mode: u16,
+     /// Low 16 bits of Owner Uid
+     uid: u16,
+     /// Size in bytes
+     size: u32,
+     /// Access time
+     atime: u32,
+     /// Creation time
+     ctime: u32,
+     /// Modification time
+     mtime: u32,
+     /// Deletion Time
+     dtime: u32,
+     /// Low 16 bits of Group Id
+     gid: u16,
+     /// Links count
+     links_count: u16,
+     /// Blocks count
+     blocks: u32,
+     /// File flags
+     flags: u32,
+     l_reserved1: u32,
+     /// Pointers to blocks
+     block: [u32; EXT2_N_BLOCKS],
+     /// File version (for NFS)
+     generation: u32,
+     /// File ACL
+     file_acl: u32,
+     /// Directory ACL
+     dir_acl: u32,
+     /// Fragment address
+     faddr: u32,
+     /// Fragment number
+     l_frag: u8,
+     /// Fragment size
+     l_fsize: u8,
+     pad1: u16,
+     l_uid_high: u16,
+     l_gid_high: u16,
+     l_reserved2: u32,
+}
+
+impl core::default::Default for INode {
+    fn default() -> INode {
+        unsafe { zeroed() }
+    }
+}
+
+impl core::fmt::Debug for INode {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> fmt::Result {
+        try!(write!(f, "INode\n"));
+        try!(write!(f, "\tFile mode: {}\n", self.mode));
+        try!(write!(f, "\tLow 16 bits of Owner Uid: {}\n", self.uid));
+        try!(write!(f, "\tSize in bytes: {}\n", self.size));
+        try!(write!(f, "\tAccess time: {}\n", self.atime));
+        try!(write!(f, "\tCreation time: {}\n", self.ctime));
+        try!(write!(f, "\tModification time: {}\n", self.mtime));
+        try!(write!(f, "\tDeletion Time: {}\n", self.dtime));
+        try!(write!(f, "\tLow 16 bits of Group Id: {}\n", self.gid));
+        try!(write!(f, "\tLinks count: {}\n", self.links_count));
+        try!(write!(f, "\tBlocks count: {}\n", self.blocks));
+        try!(write!(f, "\tFile flags: {}\n", self.flags));
+        //try!(write!(f, "\tPointers to blocks: {}\n", self.block));
+        try!(write!(f, "\tFile version (for NFS): {}\n", self.generation));
+        try!(write!(f, "\tFile ACL: {}\n", self.file_acl));
+        try!(write!(f, "\tDirectory ACL: {}\n", self.dir_acl));
+        try!(write!(f, "\tFragment address: {}\n", self.faddr));
+        try!(write!(f, "\tFragment number: {}\n", self.l_frag));
+        write!(f, "\tFragment size: {}\n", self.l_fsize)
+    }
+}
+
+pub struct DirEntry {
+    /// Inode number
+    inode: u32,
+    /// Directory entry length
+    rec_len: u16,
+    /// Name length
+    name_len: u16,
+    /// File name, up to EXT2_NAME_LEN
+    name: [u8]
+}
+
+
+/*impl core::default::Default for DirEntry {
+    fn default() -> DirEntry {
+        unsafe { zeroed() }
+    }
+}*/
+
+impl core::fmt::Debug for DirEntry {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> fmt::Result {
+        try!(write!(f, "DirEntry\n"));
+        try!(write!(f, "\tInode number: {}\n", self.inode));
+        try!(write!(f, "\tDirectory entry length: {}\n", self.rec_len));
+        write!(f, "\tName length: {}\n", self.name_len)
+        //try!(write!(f, "\tFile name, up to EXT2_NAME_LEN: {}\n", self.name));
     }
 }
